a7bfdfa4ead37703c0faa5be4a4cc11cb3cb9062
phsantana <ph_santana@outlook.com>
2019-04-14 13:21:16 -0300

    Accounts integration: success

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -42,13 +42,17 @@ class Auth {
         if (!authResult || !authResult.idToken) {
           return reject(err);
         }
-        this.idToken = authResult.idToken;
-        this.profile = authResult.idTokenPayload;
-        // set the time that the id token will expire at
-        this.expiresAt = authResult.idTokenPayload.exp * 1000;
+        this.setSession(authResult);
         resolve();
       });
     })
+  }
+
+  setSession(authResult) {
+    this.idToken = authResult.idToken;
+    this.profile = authResult.idTokenPayload;
+    // set the time that the id token will expire at
+    this.expiresAt = authResult.idTokenPayload.exp * 1000;
   }
 
   signOut() {
@@ -56,6 +60,23 @@ class Auth {
     this.idToken = null;
     this.profile = null;
     this.expiresAt = null;
+  }
+
+  signOut() {
+    this.auth0.logout({
+      returnTo: 'http://localhost:3000',
+      clientID: 'I1gha7zmap4Wz45Amy3C4GMXbPc4q5x1',
+    });
+  }
+
+  silentAuth() {
+    return new Promise((resolve, reject) => {
+      this.auth0.checkSession({}, (err, authResult) => {
+        if (err) return reject(err);
+        this.setSession(authResult);
+        resolve();
+      });
+    });
   }
 }
 
