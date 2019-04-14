5386ff4a3a7c64e64b45cf1430f325a51869880b
phsantana <ph_santana@outlook.com>
2019-04-14 13:40:22 -0300

    Application finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,14 +9,28 @@ import SecuredRoute from './SecuredRoute/SecuredRoute';
 import auth0Client from './Auth';
 
 class App extends Component {
-   async componentDidMount() {
-    if (this.props.location.pathname === '/callback') return;
+  constructor(props){
+    super(props);
+    this.state = {
+      checkingSession: true
+    };
+  }
+
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback'){
+      this.setState({checkingSession: false});
+      return;
+    }
+
     try {
       await auth0Client.silentAuth();
       this.forceUpdate();
-    } catch (err) {
+    }
+    catch (err) {
       if (err.error !== 'login_required') console.log(err.error);
     }
+
+    this.setState({checkingSession: false});
   }
   render() {
     return (
@@ -25,10 +39,15 @@ class App extends Component {
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
         <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-question' component={NewQuestion}/>
+        <SecuredRoute path='/new-question'
+                      component={NewQuestion}
+                      checkingSession={this.state.checkingSession}/>
       </div>
     );
   }
 }
 
+/* provides access for what rout is being called
+*  using: this.props.location.pathname
+*/
 export default withRouter(App);
