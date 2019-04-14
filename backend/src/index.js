42d9758abee3e539cc83729afcd83b1c7be906fd
phsantana <ph_santana@outlook.com>
2019-04-14 13:54:30 -0300

    Documenting backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 //import dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const express     = require('express');
+const bodyParser  = require('body-parser'); //convert the body of incoming requests into JSON objects
+const cors        = require('cors');
+const helmet      = require('helmet'); // helps to secure Express apps with various HTTP headers
+const morgan      = require('morgan'); // adds some logging capabilities to the Express app
+const jwt         = require('express-jwt');
+const jwksRsa     = require('jwks-rsa');
 
 // define the Express app
 const app = express();
@@ -26,22 +26,22 @@ app.use(cors());
 app.use(morgan('combined'));
 
 // retrieve all questions
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
   const qs = questions.map(q => ({
     id: q.id,
     title: q.title,
     description: q.description,
     answers: q.answers.length,
   }));
-  res.send(qs);
+  response.send(qs);
 });
 
 // get a specific question
-app.get('/:id', (req, res) => {
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
-  res.send(question[0]);
+app.get('/:id', (request, response) => {
+  const question = questions.filter(q => (q.id === parseInt(request.params.id)));
+  if (question.length > 1) return response.status(500).send();
+  if (question.length === 0) return response.status(404).send();
+  response.send(question[0]);
 });
 
 const checkJwt = jwt({
@@ -59,36 +59,34 @@ const checkJwt = jwt({
 });
 
 // insert a new question
-app.post('/', checkJwt, (req, res) => {
-  const {title, description} = req.body;
+app.post('/', checkJwt, (request, response) => {
+  const {title, description} = request.body;
   const newQuestion = {
     id: questions.length + 1,
     title,
     description,
     answers: [],
-    author: req.user.name,
+    author: request.user.name,
   };
   questions.push(newQuestion);
-  res.status(200).send();
+  response.status(200).send();
 });
 
 // insert a new answer to a question
-app.post('/answer/:id', checkJwt, (req, res) => {
-  const {answer} = req.body;
+app.post('/answer/:id', checkJwt, (request, response) => {
+  const {answer} = request.body;
 
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
+  const question = questions.filter(q => (q.id === parseInt(request.params.id)));
+  if (question.length > 1) return response.status(500).send();
+  if (question.length === 0) return response.status(404).send();
 
   question[0].answers.push({
     answer,
-    author: req.user.name,
+    author: request.user.name,
   });
 
-  res.status(200).send();
+  response.status(200).send();
 });
 
 // start the server
-app.listen(8081, () => {
-  console.log('listening on port 8081');
-});
+app.listen(8081, () => console.log('listening on port 8081'));
